7f8d1a010a42d3f58fba340be1e37252436bd7b9
alaqrasenpai <52831910+alaqrasenpai@users.noreply.github.com>
2022-10-08 13:31:10 +0300

    to trainers

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const router = require("express").Router(),
     AnswerRouter = require('./answer'),
     QuastionRouter = require('./quastion'),
     SerialsRouter = require('./serials'),
-    StudentsRouter=require("./students")
+    TrainersRouter = require('./trainers'),
+    StudentsRouter = require("./students")
 
 router.use('/answers', AnswerRouter)
 router.use('/quastions', QuastionRouter)
 router.use('/serials', SerialsRouter)
 router.use('/students', StudentsRouter)
+router.use('/trainers', TrainersRouter)
 
 module.exports = router
