1e6f17d3578d5d7e6c4edb504f36d66691a42874
alaqrasenpai <52831910+alaqrasenpai@users.noreply.github.com>
2022-10-10 23:39:28 +0300

    update index router

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,16 @@ const router = require("express").Router(),
     QuastionRouter = require('./quastion'),
     SerialsRouter = require('./serials'),
     TrainersRouter = require('./trainers'),
-    StudentsRouter = require("./students")
+    StudentsRouter = require("./students"),
+    TurialRouter = require("./tutorial"),
+    AdsRouter = require("./ads")
 
 router.use('/answers', AnswerRouter)
 router.use('/quastions', QuastionRouter)
 router.use('/serials', SerialsRouter)
 router.use('/students', StudentsRouter)
 router.use('/trainers', TrainersRouter)
+router.use('/ads', AdsRouter)
+router.use('/tutorial', TurialRouter)
 
 module.exports = router
